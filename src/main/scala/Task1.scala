51e3d814e9051ebe81773ef52ffb568bb291110f
gaborloi <gaborloi@gmail.com>
2024-12-01 21:37:03 +0100

    Task 1 done

---
+++ src/main/scala/Task1.scala
@@ -0,0 +1,26 @@
+import scala.io.BufferedSource
+
+//TODO: package
+object Task1 {
+  def parseFile1(file: BufferedSource): Int = {
+    val lines = file.getLines().toList
+    val firstOrderedList = lines.map { line =>
+      line.split(" ").head.toInt
+    }.sorted
+    val secondOrderedList = lines.map { line =>
+      line.split(" ").last.toInt
+    }.sorted
+    firstOrderedList.zip(secondOrderedList).foldLeft(0: Int) { case (s: Int, (a: Int,b: Int)) => s + math.abs(a - b) }
+  }
+
+  def parseFile2(file: BufferedSource): Int = {
+    val lines = file.getLines().toList
+    val firstOrderedList = lines.map { line =>
+      line.split(" ").head.toInt
+    }.sorted
+    val secondOrderedList = lines.map { line =>
+      line.split(" ").last.toInt
+    }.sorted
+    firstOrderedList.foldLeft(0: Int) { case (s: Int, a:Int) => s + a * secondOrderedList.count(_ == a) }
+  }
+}
